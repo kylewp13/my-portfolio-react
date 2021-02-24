708fbdd4d4e95e4586a48f1c20e45ca035107192
Kyle Porter <kylewp13@gmail.com>
2021-02-24 16:17:57 -0500

    added about, post, and single post page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import image from "../chris-dickens-b9cRz523N9c-unsplash.jpg"
 
 export default function Home() {
     return(
-        <h1>Home page</h1>
+        <main>
+            <img 
+                src={image} 
+                alt="Chicago skyline" 
+                className="absolute object-cover w-full h-full" 
+            />
+            <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+                <h1 className="text-6xl text-white cursive loading-none lg:loading-sung home-name">Hi. I'm Kyle.</h1>
+            </section>
+        </main>
     )
 };
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,65 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { SocialIcon } from "react-social-icons";
 
 export default function NavBar() {
     return(
-        <header>
-            <div>
-                <nav>
-                    <NavLink to="/" exact>
+        <header className="bg-red-600">
+            <div className="container mx-auto flex justify-between">
+                <nav className="flex">
+                    <NavLink 
+                        to="/" 
+                        exact
+                        activeClassName="text-red" 
+                        className="inflex-flex items-center py-6 px-6 mr-4 text-white text-4xl font-bold cursive tracking-widest"
+                    >
                         Kyle Hallberg
                     </NavLink>
-                    <NavLink to="/post">
+                    <NavLink 
+                        to="/post" 
+                        className="inline-flex items-center py-3 px-3 my-6 rouned text-red-200 hover:text-white" 
+                        activeClassName="text-red-100 bg-red-700"
+                    >
                         Blog Posts
                     </NavLink>
-                    <NavLink to="/projects">
+                    <NavLink 
+                        to="/projects" 
+                        className="inline-flex items-center py-3 px-3 my-6 rouned text-red-200 hover:text-white" 
+                        activeClassName="text-red-100 bg-red-700"
+                    >
                         Projects
                     </NavLink>
-                    <NavLink to="/about">
+                    <NavLink 
+                        to="/about" 
+                        className="inline-flex items-center py-3 px-3 my-6 rouned text-red-200 hover:text-white" 
+                        activeClassName="text-red-100 bg-red-700"
+                    >
                         About Me!
                     </NavLink>
                 </nav>
+                <div className="inline-flex py-3 px-3 my-6">
+                    <SocialIcon 
+                        url="https://www.linkedin.com/in/kyletech/" 
+                        className="mr-4" 
+                        target="_blank" 
+                        fgColor="#fff" 
+                        style={{ height: 35, width: 35 }} 
+                    />
+                    <SocialIcon 
+                        url="https://github.com/kylewp13" 
+                        className="mr-4" 
+                        target="_blank" 
+                        fgColor="#fff" 
+                        style={{ height: 35, width: 35 }} 
+                    />
+                     <SocialIcon 
+                        url="https://open.spotify.com/user/dx5li5aqn7q74b2hg9k2dm958" 
+                        className="mr-4" 
+                        target="_blank" 
+                        fgColor="#fff" 
+                        style={{ height: 35, width: 35 }} 
+                    />
+                </div>
             </div>
         </header>
     )
